709f1f294b9b37402b9e31226e9f8c17361b965a
caosong <caosong@baidu.com>
2017-07-31 15:18:24 +0800

    init
    Signed-off-by: caosong <caosong@baidu.com>

---
+++ CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /Volumes/Transcend/src/github.com/pkufergus/cpp-curl-cache
+# Build directory: /Volumes/Transcend/src/github.com/pkufergus/cpp-curl-cache
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(cpp-lru-cache-test "/Volumes/Transcend/src/github.com/pkufergus/cpp-curl-cache/cpp-lru-cache-test")
+subdirs("ext/gtest")
---
+++ ext/gtest/src/googletest-build/CMakeFiles/gtest.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Volumes/Transcend/src/github.com/pkufergus/cpp-curl-cache/ext/gtest/src/googletest/src/gtest-all.cc" "/Volumes/Transcend/src/github.com/pkufergus/cpp-curl-cache/ext/gtest/src/googletest-build/CMakeFiles/gtest.dir/src/gtest-all.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/Volumes/Transcend/src/github.com/pkufergus/cpp-curl-cache/ext/gtest/src/googletest/include"
+  "/Volumes/Transcend/src/github.com/pkufergus/cpp-curl-cache/ext/gtest/src/googletest"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
